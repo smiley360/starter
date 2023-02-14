17e1d402264b0f0a59c6b2429932b1e6e8b82ce9
xtopherc-vestago <christopher.chan@vesta-go.com>
2023-02-13 18:59:28 -0800

    add some other startup command

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,20 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
+echo "sleep for waiting for postgres to up"
+sleep 10
+echo "done sleeping"
 if [ ! -f /myapp/db.created ]; then
+  echo "calling into db:create"
   rails db:create
   touch /myapp/db.created
+  sleep 10
+  echo "calling into db:migrate"
+  rails db:migrate RAILS_ENV=development
 fi
+
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
+
+
