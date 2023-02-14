3c8c004121ddd338f7e84f449cea92e537da9af3
tmtarpinian <tmtarpinian@gmail.com>
2023-02-14 18:52:03 -0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/articles", to: "articles#index"
+  resources :user, controller: 'users'
+  resources :review, controller: 'reviews'
+  resources :attachment, controller: 'attachments'
 end
