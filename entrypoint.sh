f6187bae2b082d63528455f12c921341c75f6366
xtopherc-vestago <christopher.chan@vesta-go.com>
2023-02-13 19:57:20 -0800

    add some other startup command

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,18 +4,22 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
-echo "sleep for waiting for postgres to up"
-sleep 35
-echo "done sleeping"
 if [ ! -f /myapp/db.created ]; then
+  echo "sleep for waiting for postgres to up"
+  sleep 35
+  
   echo "calling into db:create"
   rails db:create
   touch /myapp/db.created
+  
   sleep 10
   echo "calling into db:migrate"
   rails db:migrate RAILS_ENV=development
+  
   sleep 10
+  echo "creating article 1"
   rails runner "Article.create(title: 'Hello Rails and Docker', body: 'I am on Rails, Postgres and Docker')"
+  echo "creating article 2"
   rails runner "Article.create(title: 'This is my 2nd title', body: 'I am happy about that')"
   #rails runner "Article.save"
 fi
